a45edb45bff1468509d9d6ef711fe81b28eb3681
Juan Manuel Sierra <juan.sierra@distillery.com>
2024-02-05 19:58:37 -0300

    Commit to test deployment

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`)
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
 })
