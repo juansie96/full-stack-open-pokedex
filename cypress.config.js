d12f1fbf88e758b10577fad635981606fac096a9
Juan Manuel Sierra <juan.sierra@distillery.com>
2024-02-05 18:03:12 -0300

    Add cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // on, config params can be added if necessary
+      // implement node event listeners here
+    },
+  },
+})
